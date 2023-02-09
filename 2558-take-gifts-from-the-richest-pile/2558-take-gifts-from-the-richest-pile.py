185a680f7e7eb27d8eb18d8d58adfff903cfb9c5
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-09 13:23:41 -0500

    Time: 50 ms (75.33%), Space: 13.9 MB (77.13%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,22 +1,13 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+
+        h = [-g for g in gifts]
+        heapify(h)
+
         for i in range(k):
+            val = heappop(h)
+            val = int(math.sqrt(-val))
+            heappush(h,-val)
             
-            #find max
-            '''
-            max_w = 0
-            max_id = 0
-            for i in range(len(gifts)):
-                if gifts[i] > max_w:
-                    max_id = i
-                    max_w = gifts[i]
-            
-            gifts[max_id] = int(math.sqrt(max_w))
-            '''
-            
-            max_w = max(gifts)
-            max_id = gifts.index(max_w)
-            gifts[max_id] = int(math.sqrt(gifts[max_id]))
-            
-        return sum(gifts)
+        return -sum(h)
             
