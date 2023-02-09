81c528f5c3e9d49630fc80ad63c3930744c64bb1
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-09 13:04:41 -0500

    Time: 160 ms (14.89%), Space: 13.9 MB (77.13%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -3,6 +3,7 @@ class Solution:
         for i in range(k):
             
             #find max
+            '''
             max_w = 0
             max_id = 0
             for i in range(len(gifts)):
@@ -11,6 +12,11 @@ class Solution:
                     max_w = gifts[i]
             
             gifts[max_id] = int(math.sqrt(max_w))
+            '''
+            
+            max_w = max(gifts)
+            max_id = gifts.index(max_w)
+            gifts[max_id] = int(math.sqrt(gifts[max_id]))
         
         return sum(gifts)
             
