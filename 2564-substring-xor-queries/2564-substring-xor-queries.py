02883caa920a668aaaf2492efbb2321dbc4011ef
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-12 13:02:07 -0500

    Time: 1431 ms (50.00%), Space: 88.9 MB (5.56%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.py
+++ 2564-substring-xor-queries/2564-substring-xor-queries.py
@@ -1,7 +1,27 @@
 class Solution:
     def substringXorQueries(self, s: str, queries: List[List[int]]) -> List[List[int]]:
         ans = []
+        mp = {}
+        for l in range(1,34):
+            for i in range(len(s)-l+1):
+                sub=s[i:i+l]
+                if sub not in mp:
+                    mp[sub] = i
         
+        for x,y in queries:
+            sub = bin(x^y)[2:]
+            
+            if sub in mp:
+                ans.append([mp[sub],mp[sub]+len(sub)-1])
+            else:
+                ans.append([-1,-1])
+        
+        return ans
+                    
+            
+        
+        
+        '''
         mp = {}
         for x,y in queries:
             sub = bin(x^y)[2:]
@@ -13,7 +33,7 @@ class Solution:
                 ans.append([-1,-1])
             else:
                 ans.append([mp[sub],mp[sub]+len(sub)-1])
-            
+        ''' 
         return ans
                     
             
