ac8c3d7c04f681e8cc5e475e724eb5cc5414d12f
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-12 13:03:23 -0500

    Time: 1483 ms (38.89%), Space: 88.8 MB (5.56%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.py
+++ 2564-substring-xor-queries/2564-substring-xor-queries.py
@@ -1,13 +1,23 @@
 class Solution:
     def substringXorQueries(self, s: str, queries: List[List[int]]) -> List[List[int]]:
-        finder = defaultdict(lambda: [-1, -1])
-        for l in range(33, 0, -1):
-            for i in range(len(s)-l, -1, -1):
-                finder[int(s[i:i+l], 2)] = [i, i+l-1]
         ans = []
-        for a, b in queries:
-            ans.append(finder[a^b])
+        mp = {}
+        for l in range(1,34):
+            for i in range(len(s)-l+1):
+                sub=s[i:i+l]
+                if sub not in mp:
+                    mp[sub] = i
+        
+        for x,y in queries:
+            sub = bin(x^y)[2:]
+            
+            if sub in mp:
+                ans.append([mp[sub],mp[sub]+len(sub)-1])
+            else:
+                ans.append([-1,-1])
+        
         return ans
+                    
             
         
         
