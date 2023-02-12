b7880e73513d846d0c8cb60174efbed9429614cc
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-12 12:05:55 -0500

    Time: 705 ms (36.36%), Space: 29 MB (18.18%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,18 +1,16 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        nums.sort()
-        n=len(nums)
+        
+        nums = sorted(nums)
+        n = len(nums)
         ans = 0
-        
-        for lo in nums:
-            #lower <= lo + up <= upper
-            #lower - lo <= up <= upper - lo
+        h,l=n-1,n-1
+        for i in range(n):
+            while h>= 0 and nums[i]+nums[h] > upper: h-=1
+            while l>= 0 and nums[i]+nums[l] >= lower: l-=1
             
-            l = bisect.bisect_left(nums, lower-lo)
-            r = bisect.bisect_right(nums, upper-lo)
-            
-            ans += r-l
-            if lower-lo<=lo<=upper-lo:
+            ans += h-l
+            if l<i<=h:
                 ans-=1
-        
+            
         return ans//2
