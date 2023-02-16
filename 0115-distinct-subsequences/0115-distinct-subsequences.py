b7d7e221e934bc65e9b5eda5029e662c8f772328
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-02-16 03:55:27 -0500

    Time: 465 ms (79.14%), Space: 14.4 MB (86.13%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        #any letter comes
+        #1. words ending till letter now
+        
+        n=len(s)
+        m=len(t)
+        
+        word_ending = {} # DONT INSERT WITOUT 0
+        for i in range(n):
+            updates=[]
+            for j in range(m):
+                if s[i]==t[j]:
+                    if j==0:
+                        updates.append((j,1))
+                    
+                    if j-1 in word_ending:
+                        updates.append((j,word_ending[j-1]))
+            
+            for idx,upd in updates:
+                if idx not in word_ending:
+                    word_ending[idx] = upd
+                else:
+                    word_ending[idx] += upd
+    
+        return word_ending[m-1] if m-1 in word_ending else 0
+                        
