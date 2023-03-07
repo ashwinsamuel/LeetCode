bac18102a9af3c82b59f3be95586447becadcd09
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-03-07 00:05:32 -0500

    Time: 2422 ms (60.75%), Space: 28.5 MB (70.25%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -3,15 +3,15 @@ class Solution:
         time = sorted(time)
         n=len(time)
         
-        #min k s.t. k//time[0] + k//time[1] + ...k//time[id] >= totalTrips
+        #min k s.t. k//time[0] + k//time[1] + ...k//time[id] >= totalTrip        
         l = 0
-        r = time[-1]*(totalTrips//n +1) if totalTrips>=n else time[totalTrips-1]
+        r = time[-1]*(totalTrips//n+1) if totalTrips>=n else time[totalTrips-1]
         while l<r:
             m=(l+r)//2
             k=bisect_right(time,m)
             trips=0
             for i in range(k):
-                trips += m//time[i]
+                trips += m//time[i]       
                 
             if trips>=totalTrips:
                 r=m
