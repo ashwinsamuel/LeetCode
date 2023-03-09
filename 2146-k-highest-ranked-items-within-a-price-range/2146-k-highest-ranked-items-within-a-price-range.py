78bb90e29f43c2c5a1a1a07f7c0e0b485e20e581
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-03-08 21:03:38 -0500

    Time: 3927 ms (28.73%), Space: 71.5 MB (10.50%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
@@ -2,26 +2,47 @@ import heapq
 from collections import deque
 
 class Solution:
-    def highestRankedKItems(self, G, pricing, start, k):
-        m, n = len(G), len(G[0])
-        row, col = start
-        node = (0, G[row][col], row, col)
-        visited = set()
-        visited.add((row, col))
-        d = deque([node])
-        ans = []
+    def highestRankedKItems(self, grid: List[List[int]], pricing: List[int], start: List[int], k: int) -> List[List[int]]:
+        #K Highest Ranked Items Within a Price Range
         
-        while d:
-            dist, cost, row, col = d.popleft()
-            if pricing[0] <= cost <= pricing[1]:
-                ans += [(dist, cost, row, col)]
-            
-            for x, y in (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1):
-                if 0 <= x <= m-1 and 0 <= y <= n-1 and (x, y) not in visited and G[x][y] != 0:
-                    d.append((dist + 1, G[x][y], x, y))
-                    visited.add((x, y))
+        def item_add(r,c,grid,items,d,up,lo):
+            if lo<= grid[r][c] <=up:
+                h = (d,grid[r][c],r,c)
+                heapq.heappush(items,h)
+            return
         
-        ans = sorted(ans)
+        #init
+        i,j = start
+        lo,up=pricing
+        items=[]
+        vis=set()
+        dist={}
+        #start should not be 0
         
-        return [[x, y] for _, _, x, y in ans[:k]]
+        #bfs
+        qu=deque()
+        qu.append((i,j))  #(i,j,vis,items,grid,dist,0)
+        
+        vis.add((i,j))
+        dist[(i,j)] = 0
+        item_add(i,j,grid,items,0,up,lo)
+        n=len(grid)
+        m=len(grid[0])
+        while qu:
+            r,c = qu.popleft()
+            for nei,nej in [(-1,0),(0,-1),(0,1),(1,0)]:
+                if 0<=r+nei<n and 0<=c+nej<m and (r+nei,c+nej) not in vis and grid[r+nei][c+nej]>0:
+
+                    ri,cj = r+nei,c+nej
+                    vis.add((ri,cj))
+                    dist[(ri,cj)] = dist[(r,c)]+1
+                    item_add(ri,cj,grid,items, dist[(ri,cj)] ,up,lo)
+                    qu.append((r+nei,c+nej))
+        
+        ans=[]
+        while k>0 and items:
+            a,b,c,d = heapq.heappop(items)
+            ans.append([c,d])
+            k-=1
+        return ans
         
