146528f4104d72e91cc87e6796c8a56478f4e5c4
ashwinsamuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-03-09 22:12:58 -0500

    Time: 7986 ms (5.15%), Space: 21 MB (58.56%) - LeetHub

--- 2532-time-to-cross-a-bridge/2532-time-to-cross-a-bridge.py
+++ 2532-time-to-cross-a-bridge/2532-time-to-cross-a-bridge.py
@@ -18,21 +18,15 @@ class Solution:
             while rpushq and rpushq[0][0]<=t:
                 _,i = heapq.heappop(rpushq)
                 heapq.heappush( rheap ,  (-time[i][0]-time[i][2],-i))
-                #print(rheap)
-            
             
             if rheap:
                 eff,i = heapq.heappop(rheap)
-                i=-i
-                bfree = t + time[i][2]
-                heapq.heappush(lpushq, (bfree+time[i][3],i))
-                print(f'{i} starts crossing from R->L at {t}')
+                bfree = t + time[-i][2]
+                heapq.heappush(lpushq, (bfree+time[-i][3],-i))
             elif n>0 and lheap:
                 eff,i = heapq.heappop(lheap)
-                eff,i=-eff,-i
-                bfree = t + time[i][0]
-                heapq.heappush(rpushq , (bfree+time[i][1],i))
-                print(f'{i} starts crossing from L->R at {t}')
+                bfree = t + time[-i][0]
+                heapq.heappush(rpushq , (bfree+time[-i][1],-i))
                 n-=1
             t=max(bfree,t+1)
 
