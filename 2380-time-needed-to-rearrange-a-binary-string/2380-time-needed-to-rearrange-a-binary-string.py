62d5f0fedd835cbb8aaa0020caa25d4579267233
Ashwin Samuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-06-16 17:49:20 -0400

    Time: 139 ms (48.16%), Space: 16.4 MB (14.69%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -4,21 +4,8 @@ class Solution:
         #cant change chr of str
         
         ans=0
-        n=len(s)
-        st = [s[i] for i in range(n)]
-        while True:
-            ok=False
-            notok=False
-            for j in range(n-1):
-                if st[j]=='0' and st[j+1]=='1' and notok==False:
-                    ok=True
-                    st[j] , st[j+1]='1','0'
-                    notok =True
-                elif notok:
-                    notok=False
-            if not ok:
-                return ans
+        while '01' in s:
+            s = s.replace('01','10')
             ans+=1
         return ans
-                
                     
