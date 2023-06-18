76cc16ae082f38c4200421298779f78f46bc6d4d
Ashwin Samuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-06-18 00:42:31 -0400

    Time: 4005 ms (33.33%), Space: 218 MB (66.67%) - LeetHub

--- 2741-special-permutations/2741-special-permutations.py
+++ 2741-special-permutations/2741-special-permutations.py
@@ -1,15 +1,17 @@
 class Solution:
     def specialPerm(self, nums: List[int]) -> int:
+        #14! * 14 or (14 nested loops)
         n, MOD = len(nums), 10**9 + 7
         @cache
-        def dfs(index, prev, mask):
+        def dfs(prev, mask):
             if mask == (1 << n) - 1: return 1
             count = 0
             for i in range(n):
                 if not (mask & (1 << i)) and (nums[i] % prev == 0 or prev % nums[i] == 0):
-                    count += dfs(index + 1, nums[i], mask | (1 << i))
+                    count += dfs(nums[i], mask | (1 << i))
             return count % MOD
-        return dfs(0, 1, 0)
+        return dfs(1, 0)
+            
         
         
         
