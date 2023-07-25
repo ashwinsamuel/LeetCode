f9213ef5ea31e8400045ddf46fdfa7629a3990e3
Ashwin Samuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-07-25 12:44:34 -0400

    Time: 2180 ms (80.21%), Space: 43.6 MB (19.79%) - LeetHub

---
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -0,0 +1,31 @@
+import heapq
+
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        
+        #exception
+        if grid[0][1]>1 and grid[1][0]>1:
+            return -1
+        
+        
+        nes = [(+1,0),(-1,0),(0,+1),(0,-1)]
+        qu = [(0,0,0)]
+        m,n = len(grid),len(grid[0])
+        vis = set([(0,0)])
+        
+        while qu:
+            time, i , j = heapq.heappop(qu)
+            
+            if i==m-1 and j==n-1:
+                return time
+            
+            for dx,dy in nes:
+                nei,nej = i+dx,j+dy
+                if 0<=nei<m and 0<=nej<n and (nei,nej) not in vis:
+                    vis.add((nei,nej))
+                    if time+1>=grid[nei][nej]:
+                        heapq.heappush(qu,(time+1,nei,nej))
+                    else:
+                        mul = (grid[nei][nej] - time)//2
+                        heapq.heappush(qu,(time+2*mul + 1,nei,nej))
+            
