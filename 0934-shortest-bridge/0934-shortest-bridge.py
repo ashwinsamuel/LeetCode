d6ab595dfc3596a90932b00efa5d20cfb4519d10
Ashwin Samuel <39940488+ashwinsamuel@users.noreply.github.com>
2023-10-08 19:14:29 -0400

    Time: 331 ms (75.18%), Space: 17.8 MB (79.04%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,53 +1,69 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        n=len(grid[0])
+
+        island_one = set()
+
+        rows, cols = len(grid), len(grid[0])
+
+        def is_valid_child(child_row, child_col):
+            return (0 <= child_row < rows) and (0 <= child_col < cols) and \
+            (child_row, child_col) not in island_one
         
-        def dfs(vis,i,j):
+        def dfs(curr_row, curr_col):
+            stack = [(curr_row, curr_col)]
+            island_one.add((curr_row, curr_col))
+
+            while stack:
+                curr_row, curr_col =  stack.pop()
+
+                #island_one.add((curr_row, curr_col))
+                possible_dir = [(curr_row+1, curr_col), (curr_row, curr_col+1), (curr_row-1, curr_col), (curr_row, curr_col-1)]
+
+                for child_row, child_col in possible_dir:
+                    if is_valid_child(child_row, child_col) and grid[child_row][child_col] == 1:
+                        stack.append((child_row, child_col))
+                        island_one.add((child_row, child_col))
             
-            vis.add((i,j))
-            for dx,dy in [(-1,0),(1,0),(0,1),(0,-1)]:
-                nei = i+dx
-                nej = j+dy
-                if 0<=nei<n and 0<=nej<n and grid[nei][nej]==1 and (nei,nej) not in vis:
-                    dfs(vis,nei,nej)
-                    
-                    
+            return
+
+
+        dfs_done = False
+        for row in range(rows):
             
-        
-        
-        island1=set()
-        ok=False
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j]==1:
-                    dfs(island1,i,j)
-                    ok=True
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    dfs(row, col)
+                    dfs_done = True
                     break
-            if ok: break
-                    
-        
-        #bfs
-        qu=deque()
-        for (i,j) in island1:
-            qu.append((i,j,0))
-        
-        #print(qu)
-        
-        vis=set()
-        while qu:
-            x,y,d = qu.popleft()
             
-            for dx,dy in [(-1,0),(1,0),(0,1),(0,-1)]:
-                nex = x+dx
-                ney = y+dy
-                if 0<=nex<n and 0<=ney<n and (nex,ney) not in island1 and (nex,ney) not in vis:
-                    
-                    if grid[nex][ney]==1:
-                        return d
-                    
-                    qu.append((nex,ney,d+1))
-                    vis.add((nex,ney))
-            #print(qu)
-        #return -1
-                    
+            if dfs_done:
+                break
         
+        # Doing BFS
+        queue = deque([val for val in island_one])
+        flip = 0
+
+        #print(queue)
+        while queue:
+            for val in range(len(queue)):
+                curr_row, curr_col = queue.popleft()
+                
+
+                # if grid[curr_row][curr_col] == 1 and (curr_row, curr_col) not in island_one:
+                #     return flip
+                
+                #island_one.add((curr_row, curr_col)) # works as a visit set
+                
+                possible_dir = [(curr_row+1, curr_col), (curr_row, curr_col+1), (curr_row-1, curr_col), (curr_row, curr_col-1)]
+
+                for child_row, child_col in possible_dir:
+                    if is_valid_child(child_row, child_col):
+                        if grid[child_row][child_col] == 1:
+                            return flip
+                        queue.append((child_row, child_col))
+                        island_one.add((child_row, child_col))
+            
+            flip += 1
+        
+
+        return -1
