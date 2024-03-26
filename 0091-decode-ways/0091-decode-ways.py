333500beccd105e548e6152aafdfcd7c9dd939bb
Ashwin Samuel <39940488+ashwinsamuel@users.noreply.github.com>
2024-03-25 22:26:04 -0400

    Time: 35 ms (66.15%), Space: 16.8 MB (17.79%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,20 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-
-        dp = [1]
-        if s[0] == "0":
-            dp.append(0)
-        else:
-            dp.append(1)
+        
+        @cache
+        def recurs(i):
             
-        for i in range(1,len(s)):
-            if s[i] == "0":
-                if s[i-1] in "12":
-                    dp.append(dp[i-1])
-                else:
-                    dp.append(0)
+            if i==n:
+                return 1
+            
+            if i<n-1 and s[i] == "2" and s[i+1] in "0123456":
+                return recurs(i+1)+recurs(i+2)
+            elif i<n-1 and s[i]=="1":
+                return recurs(i+1)+recurs(i+2)
+            elif s[i] == "0":
+                return 0
             else:
-                if s[i-1] == "1" or (s[i-1] == "2" and s[i] in "123456"):
-                    dp.append(dp[i-1] + dp[i])
-                else:
-                    dp.append(dp[i])
-
-        return dp[len(s)]
+                return recurs(i+1)
+            
+        n=len(s)
+        return recurs(0)
